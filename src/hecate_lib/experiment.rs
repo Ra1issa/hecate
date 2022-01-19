de7f0008b97689ec52ccd8860fb6c7b9e80659ae
PetitPenguin <ra1issa@bu.edu>
2022-01-19 17:12:10 -0500

    Cleanup

--- src/hecate_lib/experiment.rs
+++ src/hecate_lib/experiment.rs
@@ -3,7 +3,7 @@ use crate::hecate_lib::{
     utils,
 };
 
-const ENVELOPE_SIZE: usize = 32;
+const ENVELOPE_SIZE: usize = 128;
 
 pub fn inject_envelope_com(ctext: &[u8]) -> Vec<u8>{
     let p = platform::setup_platform();
@@ -12,12 +12,18 @@ pub fn inject_envelope_com(ctext: &[u8]) -> Vec<u8>{
     let com = utils::read_from_file::<Vec<u8>>("commitment.txt",&mut buff);
     let env = platform::sign_com(com.clone(), p.clone());
 
-    let e = bincode::serialize(&env).unwrap();
-    println!("e size {:?}", e.len());
+    let mut e = bincode::serialize(&env).unwrap().to_vec();
+    let pad_len = ENVELOPE_SIZE % 16;
+    for _ in 0.. pad_len{
+        e.push(0 as u8);
+    }
+    println!("Adding envelope");
     [ctext, &e].concat()
 }
 
 pub fn remove_envelope_com(ctext: &[u8]) -> Vec<u8>{
+    println!("Removing envelope");
     let c_len = ctext.len();
-    ctext[0..c_len-ENVELOPE_SIZE].to_vec()
+    let pad_len = ENVELOPE_SIZE % 16;
+    ctext[0..c_len-(ENVELOPE_SIZE+pad_len)].to_vec()
 }
