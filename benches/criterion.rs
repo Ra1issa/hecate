19b6f26ed35d11826aa37b4cd6923a857a6c9cea
PetitPenguin <ra1issa@bu.edu>
2022-01-26 11:54:35 -0500

    Cleanup criterion

--- benches/criterion.rs
+++ benches/criterion.rs
@@ -11,11 +11,14 @@ use hecate::{
 use std::fs;
 use core::time::Duration;
 use ed25519_dalek::{Keypair, PublicKey};
+use criterion::BenchmarkId;
 
 pub fn generate_test_parameters()-> Test {
-    let mut path = utils::get_data_path();
-    path.push("msgs/msg6.txt");
-    let msg: String= fs::read_to_string(path).unwrap();
+
+    let mut msg_vec = Vec::new();
+    let mut mfrank_vec = Vec::new();
+    let mut envelope_vec = Vec::new();
+    let mut report_vec = Vec::new();
 
     let mut rng = rand::thread_rng();
     let id = utils::random_block(32, &mut rng);
@@ -25,23 +28,36 @@ pub fn generate_test_parameters()-> Test {
     let plat_pk = (Keypair::from_bytes(&p.keypair).unwrap()).public;
 
     let token = moderator::generate_token(id.clone(), m.clone(), &mut rng);
-    let (mfrank, com) = sender::generate_frank(msg.to_string(), token.clone(), &mut rng);
-    let envelope = platform::sign_com(com.clone(), p.clone());
-    let report = receiver::check_message(mfrank.clone(), envelope.clone(), mod_pk, plat_pk);
-    let trace = moderator::inspect(report.clone(), m.clone(), plat_pk);
 
+    let msg_sizes = vec![10, 50, 100, 250, 500, 750, 1000, 2500, 5000, 7500, 10000];
+    for i in 0..msg_sizes.len(){
+        let mut path = utils::get_data_path();
+        let file = format!{"msgs/msg{:?}.txt", i};
+        path.push(file);
+
+        let msg: String= fs::read_to_string(path).unwrap();
+        msg_vec.push(msg.clone());
+
+        let (mfrank, com) = sender::generate_frank(msg.to_string(), token.clone(), &mut rng);
+        let envelope = platform::sign_com(com.clone(), p.clone());
+        let report = receiver::check_message(mfrank.clone(), envelope.clone(), mod_pk, plat_pk);
+
+        mfrank_vec.push(mfrank);
+        envelope_vec.push(envelope);
+        report_vec.push(report);
+    }
     Test{
         id,
-        msg,
+        msg: msg_vec,
         token,
-        mfrank,
-        envelope,
-        report,
-        trace,
+        mfrank: mfrank_vec,
+        envelope: envelope_vec,
+        report: report_vec,
         moderator: m,
         mod_pk: mod_pk.to_bytes().to_vec(),
         platform: p,
         plat_pk: plat_pk.to_bytes().to_vec(),
+        msg_sizes,
     }
 
 }
@@ -58,36 +74,46 @@ pub fn criterion_benchmark_moderator(c: &mut Criterion) {
 
     let plat_pk = PublicKey::from_bytes(&test.plat_pk).unwrap();
 
-    group.bench_function("Moderator :: Inspect and Trace ", |b| b.iter(||
-        moderator::inspect(
-            black_box(test.report.clone()),
-            black_box(test.moderator.clone()),
-            black_box(plat_pk),
-        )
-    ));
+    for i in 0..test.msg_sizes.len() {
+        let report = &test.report[i];
+        let msg_size =  test.msg_sizes[i];
+        group.bench_with_input(BenchmarkId::new("Inspect and Trace :: B", msg_size), &msg_size, |b,  &_s| {
+            b.iter(||
+                moderator::inspect(
+                    black_box(report.clone()),
+                    black_box(test.moderator.clone()),
+                    black_box(plat_pk),
+                )
+            );
+        });
+    }
 
     let max_time = Duration::from_secs(5);
     group.measurement_time(max_time);
 
     let batch_sizes = [1, 10, 100, 1000, 10000];
     for i in 0..batch_sizes.len(){
-        if batch_sizes[i] == 1000 {
+        let batch_size = batch_sizes[i];
+
+        if batch_size == 1000 {
             let max_time = Duration::from_secs(200);
             group.measurement_time(max_time);
-        }else if batch_sizes[i] == 10000 {
+        }else if batch_size == 10000 {
             let max_time = Duration::from_secs(1500);
             group.measurement_time(max_time);
         }
 
-        let test_name = format!{"Moderator :: Generate {:?} tokens", batch_sizes[i].to_string()};
-        group.bench_function(test_name, |b| b.iter(||
-            moderator::generate_batch(
-                black_box(batch_sizes[i]),
-                black_box(test.id.clone()),
-                black_box(test.moderator.clone()),
-                black_box(&mut rng),
-            )
-        ));
+        group.bench_with_input(BenchmarkId::new("Generate Tokens :: ", batch_size), &batch_sizes, |b,  &_s| {
+            b.iter(||
+                moderator::generate_batch(
+                    black_box(batch_sizes[i]),
+                    black_box(test.id.clone()),
+                    black_box(test.moderator.clone()),
+                    black_box(&mut rng),
+                )
+            );
+        });
+
     }
 }
 
@@ -99,27 +125,18 @@ pub fn criterion_benchmark_sender(c: &mut Criterion) {
     group.significance_level(0.1).sample_size(300);
 
 
-    let msg_sizes = [10.0, 50.0, 100.0, 250.0, 500.0, 750.0, 1.0, 2.5, 5.0, 7.5, 10.0];
-    for i in 0..msg_sizes.len(){
-
-        let mut path = utils::get_data_path();
-        let file = format!{"msgs/msg{:?}.txt", i};
-        path.push(file);
-
-        let msg: String= fs::read_to_string(path).unwrap();
-        let mut test_name = str::replace(&msg_sizes[i].to_string(),".0", "");
-        if i < 6{
-            test_name = format!{"Frank :: Msg Size {:?}B", test_name};
-        }else{
-            test_name = format!{"Frank :: Msg Size {:?}KB", test_name};
-        }
-        group.bench_function(test_name, |b| b.iter(||
-            sender::generate_frank(
-                black_box(msg.clone()),
-                black_box(test.token.clone()),
-                black_box(&mut rng),
-            )
-        ));
+    for i in 0..test.msg_sizes.len() {
+        let msg = test.msg[i].to_string();
+        let msg_size =  test.msg_sizes[i];
+        group.bench_with_input(BenchmarkId::new("Frank :: B", msg_size), &msg_size, |b,  &_s| {
+            b.iter(||
+                sender::generate_frank(
+                    black_box(msg.clone()),
+                    black_box(test.token.clone()),
+                    black_box(&mut rng),
+                )
+            );
+        });
     }
 }
 
@@ -127,39 +144,28 @@ pub fn criterion_benchmark_receiver(c: &mut Criterion) {
     let test = generate_test_parameters();
     let mut group = c.benchmark_group("Receiver");
     group.significance_level(0.1).sample_size(300);
+
     let plat_pk = PublicKey::from_bytes(&test.plat_pk).unwrap();
     let mod_pk = PublicKey::from_bytes(&test.mod_pk).unwrap();
 
-    let mut rng = rand::thread_rng();
-    let test = generate_test_parameters();
+    let max_time = Duration::from_secs(10);
+    group.measurement_time(max_time);
 
-    let msg_sizes = [10.0, 50.0, 100.0, 250.0, 500.0, 750.0, 1.0, 2.5, 5.0, 7.5, 10.0];
-    for i in 0..msg_sizes.len(){
-
-        let mut path = utils::get_data_path();
-        let file = format!{"msgs/msg{:?}.txt", i};
-        path.push(file);
-
-        let msg: String= fs::read_to_string(path).unwrap();
-        let mut test_name = str::replace(&msg_sizes[i].to_string(),".0", "");
-        if i < 6{
-            test_name = format!{"Verify :: Msg Size {:?}B", test_name};
-        }else{
-            test_name = format!{"Verify :: Msg Size {:?}KB", test_name};
-        }
-        let (mfrank, com) = sender::generate_frank(msg.to_string(), test.token.clone(), &mut rng);
-        let envelope = platform::sign_com(com, test.platform.clone());
-
-        group.bench_function(test_name, |b| b.iter(||
-            receiver::check_message(
-                black_box(mfrank.clone()),
-                black_box(envelope.clone()),
-                black_box(mod_pk),
-                black_box(plat_pk),
-            )
-        ));
+    for i in 0..test.msg_sizes.len() {
+        let mfrank = &test.mfrank[i];
+        let envelope = &test.envelope[i];
+        let msg_size =  test.msg_sizes[i];
+        group.bench_with_input(BenchmarkId::new("Verify :: B", msg_size), &msg_size, |b, &_s| {
+            b.iter(||
+                receiver::check_message(
+                    black_box(mfrank.clone()),
+                    black_box(envelope.clone()),
+                    black_box(mod_pk),
+                    black_box(plat_pk),
+                )
+            );
+        });
     }
-
 }
 
 pub fn criterion_benchmark_platform(c: &mut Criterion) {
@@ -172,7 +178,7 @@ pub fn criterion_benchmark_platform(c: &mut Criterion) {
 
     group.bench_function("Sign and Timestamp ", |b| b.iter(||
         platform::sign_com(
-            black_box(test.envelope.com.clone()),
+            black_box(test.envelope[0].com.clone()),
             black_box(test.platform.clone()),
         )
     ));
@@ -184,14 +190,22 @@ pub fn criterion_benchmark_forwarder(c: &mut Criterion) {
     group.significance_level(0.1).sample_size(300);
 
     let mut rng = rand::thread_rng();
-    group.bench_function("Forward", |b| b.iter(||
-        forwarder::forward(
-            black_box(Some(test.mfrank.clone())),
-            black_box(None),
-            black_box(test.envelope.clone()),
-            black_box(&mut rng),
-        )
-    ));
+
+    for i in 0..test.msg_sizes.len() {
+        let mfrank = &test.mfrank[i];
+        let envelope = &test.envelope[i];
+        let msg_size =  test.msg_sizes[i];
+        group.bench_with_input(BenchmarkId::new("Forward :: B", msg_size), &msg_size, |b,  &_s| {
+            b.iter(||
+                forwarder::forward(
+                    black_box(Some(mfrank.clone())),
+                    black_box(None),
+                    black_box(envelope.clone()),
+                    black_box(&mut rng),
+                )
+            );
+        });
+    }
 }
 
 
